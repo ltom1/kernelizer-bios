31cf0fa9bb7b5c52cc05d5068cab9ddb8b8f649b
ltom1 <tlechner1201@gmail.com>
2024-06-26 20:08:07 +0200

    Heap allocator fix update free value after allocation

--- src/64bit/heap.c
+++ src/64bit/heap.c
@@ -5,10 +5,12 @@
 #include <x86.h>
 
 
+// allocate <n_bytes> on the heap (bump/stack allocator)
 void *heap_alloc(heap_t *self, u64 n_bytes) {
 
     void *res = self->loc + self->top;
     self->top += n_bytes;
+    self->free -= n_bytes;
 
     if (self->top > self->size) log_err("Allocation exceeds heap size\n");
 
